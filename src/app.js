94dbc013f8824accb8d7ae81cf72ade51090b75b
kang <kcu1436@naver.com>
2021-09-21 16:59:14 +0900

    -sibal3

--- src/app.js
+++ src/app.js
@@ -44,7 +44,9 @@ app.use(morgan('dev')); // log request
 
 // express routers
 app.use('/', auth);
-app.use(express.static('public'));
+app.use('/static', express.static('public'));
+//app.use(express.static('public'));
+
 app.use('/posts', authenticateUser, posts);
 
 // api docs
