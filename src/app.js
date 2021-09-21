f7d0ff7545f44b9f52d0b9c8277336af81ec7f38
kang <kcu1436@naver.com>
2021-09-21 16:10:15 +0900

    - server 1차 배포

--- src/app.js
+++ src/app.js
@@ -6,42 +6,45 @@ import morgan from 'morgan';
 import mongoose from 'mongoose';
 import detectPort from 'detect-port';
 import chalk from 'chalk';
-
+import path from 'path';
 // api
 import auth from './api/auth.js';
 import posts from './api/posts.js';
 import docs from './utils/api-doc.js';
 
 // utils
-import { authenticateUser } from './utils/auth.js';
+import {authenticateUser} from './utils/auth.js';
 
 // mongo db
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 mongoose.connect(
-  'mongodb+srv://test:1234@cluster0-ypgh5.mongodb.net/test?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-  },
+    'mongodb+srv://test:1234@cluster0.r7hle.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+    },
 );
 mongoose.Promise = global.Promise;
 
 // server setup
 let port;
+
 async function configServer() {
-  port = 3000 || (await detectPort(3000));
+    port = 3000 || (await detectPort(3000));
 }
+
 configServer();
 
 // express setup
 const app = express();
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(morgan('dev')); // log request
 
 // express routers
-app.use('/', auth);
+//app.use('/', auth);
+app.use(express.static('public'));
 app.use('/posts', authenticateUser, posts);
 
 // api docs
@@ -49,9 +52,9 @@ app.use('/api', docs);
 
 // start
 app.listen(port, () =>
-  console.log(
-    `${chalk.white
-      .bgHex('#41b883')
-      .bold(`VUE TIL SERVER IS RUNNING ON ${port}`)}`,
-  ),
+    console.log(
+        `${chalk.white
+            .bgHex('#41b883')
+            .bold(`VUE TIL SERVER IS RUNNING ON ${port}`)}`,
+    ),
 );
